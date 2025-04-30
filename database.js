73269297dd9b6c3a074b00aa0afff38d8bd817dd
akriti-gupta27 <akritigupta8596@gmail.com>
2025-05-01 00:43:26 +0530

    mongodb 1

--- database.js
+++ database.js
@@ -1,3 +1,28 @@
-require("mongodb");
+const {MongoClient} = require("mongodb");
 
-const URI = "mongodb+srv://namastedev:fG2rMoecg5aF9tgK@namastenode.ycytt11.mongodb.net/"
+const url = "mongodb+srv://namastedev:fG2rMoecg5aF9tgK@namastenode.ycytt11.mongodb.net/";
+const client = new MongoClient(url);
+const dbName = "HelloWorld";
+async function main() {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection("User");
+
+    const data = {
+        firstName :"Deepika",
+        lastName: "Padukone",
+        city: "Mumbai"
+    }
+
+    const insertResult = await collection.insertMany([data]);
+    console.log(insertResult);
+    
+    const findResult = await collection.find({}).toArray();
+    console.log(findResult);
+    return 'done';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
